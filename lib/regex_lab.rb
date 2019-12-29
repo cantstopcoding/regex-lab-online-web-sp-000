dc5cfb3c49851d9addb0f10e6c3b909e33109351
Jonathan Koss <jonathan_koss@yahoo.com>
2019-12-29 01:53:40 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -6,7 +6,7 @@ end
 def words_starting_with_un_and_ending_with_ing(text)
    # use scan to return array
    # word starts with un && ends with ing
-   text.grep(/\A+un/)
+   text.scan(/\A+un/)
 end
 
 def words_five_letters_long(text)
