2a0ebd530055e7922722cad84127d9a6e2a1e71b
Jonathan Koss <jonathan_koss@yahoo.com>
2019-12-29 01:32:03 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,10 +1,6 @@
 def starts_with_a_vowel?(word)
   #if the word starts with a vowel(aeiou), return true 
-  if word.match(/\A[aeiou]/i) == nil
-    return false
-  else
-    return true
-  end
+  true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
