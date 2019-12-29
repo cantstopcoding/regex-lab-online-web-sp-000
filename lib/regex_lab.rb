f2af73797fb6d84d144ecbdb8e5d09e061afd328
Jonathan Koss <jonathan_koss@yahoo.com>
2019-12-29 02:00:04 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -6,7 +6,7 @@ end
 def words_starting_with_un_and_ending_with_ing(text)
    # use scan to return array
    # word starts with un && ends with ing
-   text.scan(/\A+un/, //)
+   text.scan(/\A+un/i)
 end
 
 def words_five_letters_long(text)
